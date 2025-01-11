921ce821c41e12b026a3fc7fc74ce8115f2a5f9d
PAWEENNUTpuyfai <paweennut.pp@outlook.com>
2025-01-11 18:50:02 +0700

    intitail solt

--- app/models/GameExample.scala
+++ app/models/GameExample.scala
@@ -8,7 +8,7 @@ object GameExample extends App {
     val dice = Dice()
     val player = Player(1, "Alice", Color.Blue)
     val pawn = Pawn(0, 0, player.color)
-    val currentPosition = pawn.getPosition
+    val currentPosition = pawn.getPosition()
     println(s"Player: $player")
     println(s"Initial pawn position: ${currentPosition}")
     
@@ -16,5 +16,17 @@ object GameExample extends App {
     val newPosition = pawn.move(steps)
     println(s"After rolling $steps: $newPosition")
 
-    print(s"Get new position ${pawn.getPosition}")
+    print(s"Get new position ${pawn.getPosition()}")
+    val p = Pawn(0, 0, player.color)
+    if(pawn == p){
+      println("same")
+    }else{
+      println("different")
+    }
+    val s = Slot.Start
+    println(s.block(2))
+    val n = Slot.Normal
+    println(n.block(6))
+    
+    
 }
