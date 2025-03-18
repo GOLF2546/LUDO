5e0904eb106ce0317a156fbd10c5edbf47fced3f
PAWEENNUTpuyfai <paweennut.pp@outlook.com>
2025-03-18 17:08:37 +0700

    update

--- app/models/PawnState.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-sealed abstract class PawnState
-object PawnState {
-  case object Start extends PawnState
-  case object Finish extends PawnState
-  case object End extends PawnState
-  case object Normal extends PawnState
-}
